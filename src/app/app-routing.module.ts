8f1d31a73589226f761dee458f9e12c781619eee
sunkers <mougin.loic@gmail.com>
2023-04-18 13:41:55 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 // import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [{ path: '', component: AppComponent },  {
-  path: 'page2',
-  loadChildren: () =>
-    import('./features/home/page2/page2.module').then((m) => m.Page2Module),
-},];
+const routes: Routes = [{ path: '', component: AppComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,20 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ThemeSwitchComponent } from './core/theme-switch/theme-switch.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './features/home/home.component';
 import { AboutComponent } from './features/about/about.component';
 import { FaqComponent } from './features/faq/faq.component';
 import { BlogComponent } from './features/blog/blog.component';
 import { ContactComponent } from './features/contact/contact.component';
-
 import { ChartModule } from 'primeng/chart';
 import { CardHomeComponent } from './features/home/card-home/card-home.component';
 import { Page2Component } from './features/home/page2/page2.component';
 import { Page3Component } from './features/home/page3/page3.component';
-import { AboutModule } from './features/about/about.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 
 @NgModule({
   declarations: [
@@ -47,7 +45,6 @@ import { AboutModule } from './features/about/about.module';
     HttpClientModule,
     AngularSvgIconModule.forRoot(),
     MatIconModule,
-    // AboutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
